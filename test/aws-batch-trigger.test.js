f440f3ba8d39c4a2631ca1b5ca6e537d35f80e2c
Adriean Khisbe <adriean.khisbe@live.fr>
2017-09-15 17:05:29 +0200

    init: write a mocked test of aws batch sdk

--- test/aws-batch-trigger.test.js
+++ test/aws-batch-trigger.test.js
@@ -1,12 +1,21 @@
 const test = require('ava');
+const AWS = require('aws-sdk-mock');
 const LambdaTester = require('lambda-tester');
 
 const {handler} = require('..');
 
+const mockSubmitJob = mockFunction => AWS.mock('Batch', 'submitJob', mockFunction);
+
 test.beforeEach(t => {
   t.context.lambda = LambdaTester(handler);
 });
+test.afterEach(t => {
+  AWS.restore('Batch');
+});
 
-test('Hello world, handler', t => {
-  t.truthy('Mocking to be set up');
+test('Broken AWS Batch', t => {
+  mockSubmitJob((param, cb) => cb(new Error('BOOM')));
+  return t.context.lambda
+    .event({jobName: 'test', jobQueue: 'queue', jobDefinition: ' jobdef'})
+    .expectError(err => t.deepEqual(err.message, 'BOOM'));
 });
