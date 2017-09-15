779c685a99c7d04d90873d3e70336f0ec46298a7
Adriean Khisbe <adriean.khisbe@live.fr>
2017-09-15 17:58:51 +0200

    dev: draft of a kinesis trigger job

--- index.js
+++ index.js
@@ -1,10 +1,40 @@
 const AWS = require('aws-sdk');
 
+const parseEvent = event => {
+  let request;
+  const records = event.Records;
+  if(records) {
+    if(records.length !== 1) {
+      return new Error(`Invalid payload format. ${records.length} records. must contain single item.`)
+    }
+    const record = records[0];
+    if(record.eventSource !== 'aws:kinesis') {
+      return new Error(`Event source ${record.eventSource} not supported`);
+    }
+    const payload = new Buffer(record.kinesis.data, 'base64').toString('utf-8')
+    try {
+      request = JSON.parse(payload);
+    } catch(err) {
+      return new Error('Kinesis Payload is not a json');
+    }
+  } else {
+    request = event;
+  }
+  //
+
+  return request;
+}
+
 exports.handler = (event, context, callback) => {
   const batch = new AWS.Batch({apiVersion: '2016-08-10'});
 
-  const jobDefinition = event;
-  batch.submitJob(jobDefinition, (err, res) => {
+  const jobRequest = parseEvent(event);
+  if (jobRequest instanceof Error) {
+    console.error(jobRequest);
+    return callback(jobRequest);
+  }
+
+  batch.submitJob(jobRequest, (err, res) => {
     if (err) {
       console.error(err);
       return callback(err);
