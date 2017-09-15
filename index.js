2e92f31f480cfc7cd142aa62a0e14d2d6bdaaef0
Adriean Khisbe <adriean.khisbe@live.fr>
2017-09-15 16:19:28 +0200

    init: apply lint fix (prettier)

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const AWS = require('aws-sdk');
 
-
 exports.handler = (event, context, callback) => {
   // TODO implement
   callback(null, 'Hello from Lambda');
--- test/aws-batch-trigger.test.js
+++ test/aws-batch-trigger.test.js
@@ -5,11 +5,10 @@ const {handler} = require('..');
 
 test.beforeEach(t => {
   t.context.lambda = LambdaTester(handler);
-})
+});
 
 test('Hello world, handler', t => {
-  return t.context.lambda.event({})
-    .expectResult(res => {
-      t.truthy(/hello/i.test(res));
-    })
+  return t.context.lambda.event({}).expectResult(res => {
+    t.truthy(/hello/i.test(res));
+  });
 });
