83d1e01b64078c357e466ad6717c7665dc82fa10
Adriean Khisbe <adriean.khisbe@live.fr>
2017-09-18 15:11:40 +0200

    refactor: reformat the files

--- index.js
+++ index.js
@@ -23,27 +23,27 @@ exports.handler = (event, context, callback) => {
 };
 
 const parseEvent = event => {
-  const request = (event.Records) ? handleAwsTrigger(event.Records): event;
+  const request = event.Records ? handleAwsTrigger(event.Records) : event;
   //
   return request;
-}
+};
 
-const handleAwsTrigger = (records) => {
-  if(records.length !== 1) {
-    throw new Error(`Invalid payload format. ${records.length} records. must contain single item.`)
+const handleAwsTrigger = records => {
+  if (records.length !== 1) {
+    throw new Error(`Invalid payload format. ${records.length} records. must contain single item.`);
   }
   const record = records[0];
   if (record.eventSource === 'aws:kinesis') {
     return handleKinesisRecord(record);
-  } else if(record.EventSource === 'aws:sns') {
+  } else if (record.EventSource === 'aws:sns') {
     return handleSnsRecord(record);
   } else {
     throw new Error(`Event source ${record.eventSource || record.EventSource} not supported`);
   }
-}
+};
 
 const handleKinesisRecord = record => {
-  const payload = new Buffer(record.kinesis.data, 'base64').toString('utf-8')
+  const payload = Buffer.from(record.kinesis.data, 'base64').toString('utf-8');
   try {
     return JSON.parse(payload);
   } catch (err) {
